232edba4ec2c17d3ffae12a975da4d1ba54600df
nutthanon <nutthanon.tho@gmail.com>
2024-04-21 11:25:39 +0700

    Initial Commit

---
+++ CTFTimeApp/CTFTimeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CTFTimeAppApp.swift
+//  CTFTimeApp
+//
+//  Created by Nutthanon on 21/4/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct CTFTimeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
